c9bb4a7fc9799b4b2cad3c4a23a3cebd36bf7790
Khushwant Singh <khushbaba@gmail.com>
2016-09-14 18:11:11 +0530

    db connection established successfully.

---
+++ db_create.sql
@@ -0,0 +1,39 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 14, 2016 at 10:21 AM
+-- Server version: 5.5.32
+-- PHP Version: 5.4.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `search`
+--
+
+CREATE DATABASE IF NOT EXISTS `search` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `search`;
+
+-- --------------------------------------------------------
+
+-- Table structure for table `user`
+
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `serial_no` int(13) NOT NULL AUTO_INCREMENT,
+  `first_name` char(50) NOT NULL,
+  `last_name` char(50) NOT NULL,
+  `full_name` char(100) NOT NULL,
+  PRIMARY KEY (`serial_no`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
